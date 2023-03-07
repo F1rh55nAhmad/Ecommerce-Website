8d3df4ea4baedd1ace221fd14b955095d34fdabc
F1rh55nAhmad <farheenahmad0056@gmail.com>
2023-03-08 01:34:08 +0500

    MVC structure and server added

---
+++ server.js
@@ -0,0 +1,12 @@
+const express =require('express')
+const app = express();
+
+const routes = require('./routers/router');
+
+ app.use('/', routes)
+
+app.listen(3002,()=>{
+    console.log('Server started on port 3002');
+});
+
+
