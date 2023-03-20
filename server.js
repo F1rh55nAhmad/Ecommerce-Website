a3cdb22b51e4139a4346a1330e483e06d826a6e4
ZaraShakeel <125494115+ZaraShakeel@users.noreply.github.com>
2023-03-20 11:42:38 +0500

    This is commit from zara

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require('./routers/router');
  app.use('/', routes)
 
 app.listen(3002,()=>{
-    console.log('Server started on port 3002');
+    console.log('Server started on port 3002'); // this is comment from zara
 });
 
 
